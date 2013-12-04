13aa23f07230e4cb48652c1376f3e0dcc5564fd2
Paul Munday <contactme@paulmunday.net>
2013-12-04 10:19:35 -0800

    For view source usibng pygments. Modified base.html to allow for internal css. Moved static pages to own source files in src. Added appropriate changes to views.py

--- resumemaker/views.py
+++ resumemaker/views.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
 
-import unittest
 from flask import render_template, flash, redirect
 from flask import Markup 
+
+from pygments import highlight
+from pygments.lexers import PythonLexer
+from pygments.formatters import HtmlFormatter
+from pygments.styles import get_style_by_name
+
+import os.path
+from os import getcwd
+
 from resumemaker import app
+
+path = os.path.join(getcwd(), 'resumemaker')
+srcpath = os.path.join(path, 'src')
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
@@ -11,26 +22,29 @@ def index():
         title = "resume", heading = "Paul Munday", 
     contents = Markup("""My Resume will be here..."""))
 
+@app.route("/src")
+def source():
+    with open(os.path.join(path, 'views.py'), 'r') as f:
+        src = f.read()
+    code = highlight(src, PythonLexer(), HtmlFormatter())
+    css = HtmlFormatter(style="friendly").get_style_defs('.highlight')
+    return render_template("static.html", internal_css =  css,
+            title = "view the source", heading = "View the Source",
+            contents = Markup(code))
+
 @app.route("/about/")
 def about():
+    with open(os.path.join(srcpath, 'about'), 'r') as f:
+        src = f.read()
     return render_template("static.html", 
         title = "about", heading = "About This Website", 
-    contents = Markup("""Content goes here..."""))
+    contents = Markup(src))
 
 @app.route("/colophon/")
 def colophon():
+    with open(os.path.join(srcpath, 'colophon'), 'r') as f:
+        src = f.read()
     return render_template("static.html", 
         title = "colophon", heading = "Colophon", 
-    contents = Markup("""<h3>A note about the technology</h3><em>This was made using the <a href="http://flask.pocoo.org">Flask</a> microframework for <a href="http://www.python.org">Python</a>.</em><br> 
-    <h3>A note about the type</h3>
-    <em>This is set in the <a href="http://www.google.com/fonts/specimen/EB+Garamond">EB Garamond</a> typeface.</em>"""))
+    contents = Markup(src))
 
-@app.route("/src")
-def source():
-    with open('/home/paul/code/paulmunday.net/resumemaker/views.py', 'r') as f:
-        contents = f.read()
-    return render_template("static.html",
-            title = "view the source", heading = "View the Source",
-            contents = Markup("<code> %s </code>" % contents))
-
-    #if __name__ == "__main__":
