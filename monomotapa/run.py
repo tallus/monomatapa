8eb9439eecc6822bfbc7f0f9b91f3c226606d67f
Paul Munday <contactme@paulmunday.net>
2014-01-17 15:19:44 -0800

    removed app.debug=True

--- monomotapa/run.py
+++ monomotapa/run.py
@@ -2,5 +2,5 @@
 
 from monomotapa import app
 if __name__ == "__main__": 
-    app.debug = True   # for dev purposes only, never production
+    # app.debug = True   # for dev purposes only, never production
     app.run()
